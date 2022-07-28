55e3741a554f0b427a89de92e618b4dd916f1d04
Shak49 <shakfeizi@gmali.com>
2022-07-28 11:10:54 -0500

    initial project files

---
+++ GoodWeather_Exp/GoodWeather_Exp/GoodWeather_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeather_ExpApp.swift
+//  GoodWeather_Exp
+//
+//  Created by Shak Feizi on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeather_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
